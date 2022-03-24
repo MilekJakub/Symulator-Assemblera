11b0856a20e14cbb9fc83870eeda05916ce5da6a
Jakub Miłek <Jakub.Milek@microsoft.wsei.edu.pl>
2022-03-24 14:43:33 +0100

    UI

--- Symulator Assemblera/MainWindow.xaml.cs
+++ Symulator Assemblera/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Symulator_Assemblera
 {
@@ -23,6 +24,11 @@ namespace Symulator_Assemblera
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
