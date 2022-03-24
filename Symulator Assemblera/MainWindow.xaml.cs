73abefa6eb619e734578f84b464ee9a5abd9285f
Jakub Miłek <Jakub.Milek@microsoft.wsei.edu.pl>
2022-03-25 00:18:25 +0100

    MOV implementation

--- Symulator Assemblera/MainWindow.xaml.cs
+++ Symulator Assemblera/MainWindow.xaml.cs
@@ -20,15 +20,47 @@ namespace Symulator_Assemblera
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
             movButton.Click += MovButton_Click;
+            xchgButton.Click += XchgButton_Click;
+        }
+
+        private void XchgButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not implemented");
         }
 
         private void MovButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            string ax = ParseInput(axInput.Text);
+            string bx = ParseInput(bxInput.Text);
+            string cx = ParseInput(cxInput.Text);
+            string dx = ParseInput(dxInput.Text);
+            axValue.Text = ax;
+            bxValue.Text = bx;
+            cxValue.Text = cx;
+            dxValue.Text = dx;
+        }
+        private string ParseInput(string input)
+        {
+            int temp;
+            if (String.IsNullOrWhiteSpace(input))
+                input = "0";
+
+            try
+            {
+                temp = int.Parse(input, System.Globalization.NumberStyles.HexNumber);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"{input} is not a valid number.", "Invalid value", MessageBoxButton.OK, MessageBoxImage.Error);
+                temp = 0;
+            }
+
+            return input = temp.ToString();
         }
     }
 }
